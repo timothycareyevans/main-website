8cbf4e41ed270b3fa1883bf038d2612631774c7b
timothycareyevans <timothycareyevans@gmail.com>
2026-07-08 15:26:25 -0700

    Add quit option and validate player input
    Handle 'q' to quit the game and validate player moves. The input loop now exits only when the player enters 'r', 'p', or 's', prints an error for other inputs, and calls sys.exit() on 'q' to terminate the program.

--- practice/rpsGame.py
+++ practice/rpsGame.py
@@ -12,3 +12,8 @@ while True: # The main game loop
     while True: # The player input loop
         print('Enter your move: (r)ock (p)aper (s)cissors or (q)uit')
         player_move = input('>')
+        if player_move =='q':
+            sys.exit() #Quit the program.
+        if player_move == 'r' or player_move == 'p' or player_move == 's':
+            break # Break out of the player input loop.
+        print('Type one of r, p, s, or q.')
